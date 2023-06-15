317d848e58f516ae7db8536022f1c8e5400caf3c
mukiza111 <dominiquemukiza@gmail.com>
2023-06-15 06:20:11 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/*
+ * main -Entry point
+ *
+ * Description: A C program that print puts function
+ *
+ * Return: always 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
