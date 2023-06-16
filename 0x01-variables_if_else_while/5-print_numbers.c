7c1a6281d07a6784cda86815bf834f957a5bc6ba
mukiza111 <dominiquemukiza@gmail.com>
2023-06-16 04:29:46 -0700

    vv

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+*main -Entry point
+*Description: 'Check for numbers if it is positive or negative'
+*Return: always 0
+*/
+int main(void)
+{
+int n;
+for (n == 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
