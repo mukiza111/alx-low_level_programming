259659f96325262eb5542673da63e2b2cceef352
mukiza111 <dominiquemukiza@gmail.com>
2023-07-07 04:40:53 -0700

    llkkk

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,9 @@ int _strlen_recursion(char *s);
  */
 int is_palindrome(char *s)
 {
-        if (*s == 0)
-                return (1);
-        return (check_pal(s, 0, _strlen_recursion(s)));
+if (*s == 0)
+return (1);
+return (check_pal(s, 0, _strlen_recursion(s)));
 }
 /**
  * _strlen_recursion - returns the length of a string
@@ -21,9 +21,9 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-        if (*s == '\0')
-                return (0);
-        return (1 + _strlen_recursion(s + 1));
+if (*s == '\0')
+return (0);
+return (1 + _strlen_recursion(s + 1));
 }
 /**
  * check_pal - checks the characters recursively for palindrome
@@ -35,10 +35,9 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int i, int len)
 {
-        if (*(s + i) != *(s + len - 1))
-                return (0);
-        if (i >= len)
-                return (1);
-        return (check_pal(s, i + 1, len - 1));
+if (*(s + i) != *(s + len - 1))
+return (0);
+if (i >= len)
+return (1);
+return (check_pal(s, i + 1, len - 1));
 }
-
